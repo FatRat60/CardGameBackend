19b188ec4e889c694a9e938bdc0787fdc9190482
FatRat60 <kylehultgren99@yahoo.com>
2023-03-22 12:20:36 -0700

    Backend listens for sockets now

--- backend.js
+++ backend.js
@@ -2,7 +2,13 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const app = express();
+const http = require('http');
 const port = 6969;
+const ioport = 9696;
+const socketIO = require('socket.io');
+let server = http.createServer(app);
+let io = socketIO(server);
+
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -25,7 +31,6 @@ app.post("/login", async (req, res) => {
     if (retrievedUser && retrievedUser.username != undefined) {
         // just compare password for now. Will decyrpt later
         bcrypt.compare(password, retrievedUser.password, function(err, result) {
-            console.log("%s and %s", password, retrievedUser.username);
             if (result){
                 console.log("success");
                 res.status(200).send(retrievedUser);
@@ -52,7 +57,6 @@ app.post("/signup", async (req, res) => {
             bcrypt.hash(newUser.password, saltRounds, async function(err, hash) {
                 newUser.password = hash;
                 const savedUser = await userServices.addUser(newUser);
-                console.log(savedUser);
                 if (!savedUser) res.status(500).end();
                 else res.status(201).send(savedUser);
             });
@@ -80,9 +84,7 @@ app.post("/updateUser", async (req, res) => {
         if (user_search.length == 0) res.status(400).send("User does not exist");
         else {
             newUser.decks = user_search[0].decks;
-            console.log(newUser.decks);
             const savedUser = await userServices.updateUser(newUser);
-            console.log(savedUser);
             if (!savedUser) res.status(500).end();
             else res.status(201).send();
         }
@@ -103,15 +105,24 @@ const calculateOrderAmount = (items) => {
 app.post("/payment", async (req, res) => {
     const { items } = req.body;
     console.log("paying");
+    const customer = await stripe.customers.create();
+    const ephKey = await stripe.ephemeralKeys.create(
+        {customer: customer.id},
+        {apiVersion: '2022-11-15'}
+    );
     const paymentIntent = await stripe.paymentIntents.create({
         amount: calculateOrderAmount(items),
         currency: "usd",
+        customer: customer.id,
         payment_method_types: ['card'],
     });
 
     res.send({
         clientSecret: paymentIntent.client_secret,
+        ephemeralKey: ephKey.secret,
+        customer: customer.id
     });
+    console.log("sent intent");
 });
 
 app.post("/store", async (req, res) => {
@@ -124,6 +135,18 @@ app.post("/store", async (req, res) => {
     }
     console.log(storeItems.length);
     res.status(200).send(storeItems);
+});
+
+io.on('connection', (socket) => {
+    console.log("A user connected");
+    socket.on('disconnect', () => {
+        console.log("user had disconnected");
+    })
+    // TODO: implement logic
+});
+
+server.listen(ioport, () => {
+    console.log("socket.io is listening on port " + ioport+ ".");
 });
 
 app.listen(port, () => {
